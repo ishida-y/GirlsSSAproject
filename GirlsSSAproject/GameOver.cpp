b22d42189f4bf006e470518524fd7b699528996e
Toshiaki <toshiaki@chiba-u.jp>
2017-12-26 08:04:37 +0900

    GameOverでタイトルにもどるのだけ書いた

--- GirlsSSAproject/GameOver.cpp
+++ GirlsSSAproject/GameOver.cpp
@@ -1,13 +1,16 @@
 # include <Siv3D.hpp>
 # include <HamFramework.hpp>
 # include "GameOver.h"
+#include"System.h"
 
 void GameOver::init() {
 
 }
 void GameOver::update() {
-
+	if (GameSystem::get().input.buttonA.clicked) {
+		changeScene(L"Title");
+	}
 }
 void GameOver::draw() const {
-
+	Println(L"Game Over:A‚Åƒ^ƒCƒgƒ‹‰æ–Ê‚Ö");
 }
